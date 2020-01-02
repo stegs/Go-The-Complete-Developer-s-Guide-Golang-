f38540ff33e24d2fd4a45b5245c1113c22e18318
Ryan Watson <ryan.watson@mlse.com>
2020-01-02 10:15:20 -0500

    Assignment 1 - version 2

--- assignment1/main.go
+++ assignment1/main.go
@@ -2,19 +2,26 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
 	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
-	for i := range nums {
+	/*for i := range nums {
 		if math.Mod(float64(nums[i]), 2) == 0 {
 			fmt.Println(i, "even")
 		}
 
 		if math.Mod(float64(nums[i]), 2) != 0 {
 			fmt.Println(i, "odd")
+		}
+	}*/
+
+	for _, num := range nums {
+		if num%2 == 0 {
+			fmt.Println(num, "even")
+		} else if num%2 != 0 {
+			fmt.Println(num, "odd")
 		}
 	}
 }
